4c495378b6745e4732e19a98b57c602a610e8c57
NathanielRijndorp <xxwolfleader2xx@gmail.com>
2022-11-16 22:10:54 +0800

    Exercise 1.

---
+++ exercise_1.py
@@ -0,0 +1,7 @@
+name = input("Enter Username: ")
+age = int(input("Enter Age: "))
+firstNum = int(input("Enter the first number: "))
+secondNum = int(input("Enter the second number: "))
+product = firstNum * secondNum
+print("The username is", name, "and the age is", age)
+print("The product is", product)
